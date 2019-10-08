656e21fc60111d9d7fb6fe482993c2116c6e4cd3
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-10-08 18:51:48 +0100

    we have life off

---
+++ src/main/java/com/codceclan/example/FoldersFiles/FoldersFilesApplication.java
@@ -0,0 +1,13 @@
+package com.codceclan.example.FoldersFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldersFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldersFilesApplication.class, args);
+	}
+
+}
